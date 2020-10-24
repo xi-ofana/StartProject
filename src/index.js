cad95534752f14876c631597e3df295e449ce984
xi-ofana <gso.santosTW@gmail.com>
2020-10-24 12:46:47 -0300

    Tela Home incompleta

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import * as jquery from 'jquery/dist/jquery';
+
+
+function Mudarestado(el) {
+    var display = document.getElementById(el).style.display;
+    
+    if (display == "none")
+      document.getElementById(el).style.display = 'block';
+    else
+      document.getElementById(el).style.display = 'none';
+  }
