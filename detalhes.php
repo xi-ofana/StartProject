72907325b729e59ffa7664f046c09eeeab66db9b
xi-ofana <53344953+xi-ofana@users.noreply.github.com>
2020-12-05 23:56:27 -0300

    Lista

--- detalhes.php
+++ detalhes.php
@@ -1,4 +1,25 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+	<meta charset="utf-8">
+	 <!-- Required meta tags -->
+	 <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+	
+	<link rel="stylesheet" href="src/css/main.css">
+
+		<title>
+			Startup
+		</title>
+
+	</head>
+	<body>
+
+
+	<?php
 	require_once "conexao.php";
 	//print_r($_GET);
 	$id_startup = $_GET["startup"];
@@ -9,18 +30,24 @@
 	$dados = mysqli_fetch_array($executa);
 
 	
-	echo "		
-		<p><img src='logotipos/$dados[logotipo]' width='35%'/></p>
-		<p><b>Razão Social:</b>$dados[nome]</p>		
-		<p>$dados[razao_social]</p>
-		<p><b>CNPJ:</b>$dados[cnpj]</p>
-		<p>$dados[segmento]</p>
-		<p>$dados[endereco]</p>
-		<p><b>Cidade:</b>$dados[cidade]</p>	
-		<p>$dados[data_fundacao]</p>	
-		<p>$dados[numero_funcionarios]</p>	
-		<p>$dados[telefone]</p>	
-		<p>$dados[descricao]</p>	
+	echo "		<div class='row' style='background-color='white'>
+	
+	<p><img src='logotipos/$dados[logotipo]' width='35%'/></p>
+	<p><b>Razão Social: </b>$dados[nome]</p>		
+	<p>$dados[razao_social]</p>
+	<p><b>CNPJ: </b>$dados[cnpj]</p>
+	<div class='d-flex'><b>Segmento: </b><p>$dados[segmento]</p></div>
+	<p>$dados[endereco]</p>
+	<p><b>Cidade: </b>$dados[cidade]</p>	
+	<p>$dados[data_fundacao]</p>	
+	<p>$dados[numero_funcionarios]</p>	
+	<p>$dados[telefone]</p>	
+	<p>$dados[descricao]</p>	
+
 	";
 ?>
-	</table>
+<a href="lista.php" class='btn btn-danger'>Voltar</a>
+</div>
+	
+	</body>
+</html>
--- lista.php
+++ lista.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<html lang="pt-br">
 	<head>
 	<meta charset="utf-8">
 	 <!-- Required meta tags -->
@@ -7,53 +8,166 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+	
+	<link rel="stylesheet" href="src/css/main.css">
 
-
-	<title> Catálogo de startups </title>
+	<title> Catálogo </title>
 
 	<body>
+      <!--
+        Pre-loader
+      -->
+      <!-- início do preloader -->
+      <div id="preloader">
+          <div class="inner">
+            <!-- HTML DA ANIMAÇÃO MUITO LOUCA DO SEU PRELOADER! -->
+            <div class="bolas">
+                <div></div>
+                <div></div>
+                <div></div>                    
+            </div>
+          </div>
+      </div>
+      <!--- TODO O HEADER (CABEÇALHO) DA PÁGINA --->
+      <header class="pb-2 mb-2 pb-lg-1 pb-xl-1 mb-lg-1 mb-xl-3 mb-sm-3 pb-sm-5"id="topo">
+              <nav class="navbar fixed-top navbar-expand-lg d-inline-flex " role="navigation">
+                  
+                    
+                    <button class="navbar-toggler justify-content-center" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" 
+                    aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+                    <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-list" fill="whitesmoke" xmlns="http://www.w3.org/2000/svg">
+                      <path fill-rule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                    </svg>
+                    </button>
+
+                    <div class=" collapse navbar-collapse" id="conteudoNavbarSuportado">
+
+                          <ul class="navbar-nav mr-auto">
+                            <button onclick="document.location='index.html'" id="button_head">
+                            <li class="navbar-item p-2 mx-auto">
+                              <svg width="40px" height="40px" viewBox="0 0 16 16" class="bi bi-house-door-fill" fill="#8ad6ee" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M6.5 10.995V14.5a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .146-.354l6-6a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 .146.354v7a.5.5 0 0 1-.5.5h-4a.5.5 0 0 1-.5-.5V11c0-.25-.25-.5-.5-.5H7c-.25 0-.5.25-.5.495z"/>
+                                <path fill-rule="evenodd" d="M13 2.5V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+                              </svg><br>
+                              StartWeb</li></button>
+
+                        <button onclick="document.location='sobre.html'" id="button_head" >
+                          <li class="nav-item m-1 p-1">
+                            <svg width="40px" height="40px" viewBox="0 0 16 16" class="bi bi-file-easel-fill" fill="#8ad6ee" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM8.5 5a.5.5 0 1 0-1 0h-2A1.5 1.5 0 0 0 4 6.5v2A1.5 1.5 0 0 0 5.5 10h.473l-.447 1.342a.5.5 0 1 0 .948.316L7.027 10H7.5v1a.5.5 0 0 0 1 0v-1h.473l.553 1.658a.5.5 0 1 0 .948-.316L10.027 10h.473A1.5 1.5 0 0 0 12 8.5v-2A1.5 1.5 0 0 0 10.5 5h-2zM5 6.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-5a.5.5 0 0 1-.5-.5v-2z"/>
+                            </svg><br>Sobre
+                         </li>
+                        </button>
+                        <button onclick="document.location='lista.php'" id="button_head" >
+                          <li class="nav-item m-1 p-1"> 
+                            <svg width="40px" height="40px" viewBox="0 0 16 16" class="bi bi-file-play-fill" fill="#8ad6ee" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM6 5.883v4.234a.5.5 0 0 0 .757.429l3.528-2.117a.5.5 0 0 0 0-.858L6.757 5.454a.5.5 0 0 0-.757.43z"/>
+                            </svg><br>Catálogo
+                          </li>
+                        </button>
+                        
+
+                      
+                      </ul>
+                  </div>
+              </nav>
+        
+		</header>
+		
 
 
+
+        <!-- 
+			AQUI VAI O CONTEÚDO
+		-->
+		<div class="row mt-5 pt-5">
+       
+		</div>
 <?php
 	require_once "conexao.php";
-	$query="SELECT id_startup, nome, descricao, logotipo FROM startup ORDER BY nome ASC";
+	$query="SELECT id_startup, nome, descricao, segmento, logotipo, cidade, data_fundacao FROM startup ORDER BY nome ASC";
 	$executar = mysqli_query($conexao,$query);
 ?>
-	<table border='1'>
-		<tr>
-			<td>Nome</td>
-			<td>Ver Detalhes</td>
-		</tr>
+
 <?php
 	while($dados = mysqli_fetch_array($executar)){
         
 		echo "
-		
-       
-
-		<ul class='list-unstyled'>
-  <li class='media'>
-    <img class='mr-3' src='logotipos/$dados[logotipo]' width='150px' height='140px' alt='logo'>
-	<div class='media-body'>
-      <h5 class='mt-0 mb-1'>$dados[nome]</h5>
-	  $dados[descricao]
-	 <p> <p> <p> <p> <a href='detalhes.php?startup=$dados[id_startup]' class='btn btn-primary'>Visitar</a> </p> </p> </p> </p>
-	 <br> <br> <br>
-	 </div>
-
-  </li>
-
-	
+			<div class='row mb-5 mr-2 ml-3 p-5 m-lg-5 m-xl-5 m-sm-5 p-lg-5 p-xl-5 p-sm-5 ' style='background-color: white;'>
+				<div class='col-12 col-lg-4 col-xl-4 col-sm-4'>
+					<img class='rounded mx-auto img-thumbnail m-3 ' width='300px' height='200px' src='logotipos/$dados[logotipo]'  alt='logo'>
+				</div>
+				<div class='col-12 col-lg col-xl col-sm'>
+					<h5 class=' display-4 mb-4' style='font-size: 45px; color: #378fac'>$dados[nome]
+					</h5>
+					<div class='d-flex'>
 					
-		";
+					<br>
+				  		<p class='text-muted'>$dados[segmento]</p>
+	  						<p class='text-muted'>/</p>
+							  	<p class='text-muted'>$dados[cidade]</p>
+					</div>
+					<hr>
+					
+					<p class='text'> $dados[descricao]</p>
+					
+					<p> <p> <p> <p> 
+					<hr>
+					<a href='detalhes.php?startup=$dados[id_startup]' class='btn btn-entrar mt-1'>Conhecer</a> </p> </p> </p> </p>
+					
+				</div>
+			</div>
+						";
 	}
 ?>
-	</table>
+<hr>
+		<!--
+          RODAPÉ
+        -->
+        <footer class="container-footer mx-0">
+          <div class="row align-items-center">
+            <div class="col"></div>
+                <div class="col align-items-center m-3">
+                  <a href="https://iconscout.com/illustrations/collaborate" target="_blank">Collaborate Illustration</a> by <a href="https://iconscout.com/contributors/pixel-true-designs">Pixel True</a> on <a href="https://iconscout.com">Iconscout</a>
+                </div>
+            <div class="col"></div>
+              </div>
+              <div class="row align-items-center">
+                <div class="col"></div>
+                  <div class="col align-items-center m-3">
+                  <span class="text-monospace"> 
+                    Desenvolvido por TR3S GATINHAX(^･ｪ･^) - 2020
+                  </span>
+                </div>
+                <div class="col"></div>
+              </div>
+              
+              
 
+           <!--
+          BOOTSTRAP LINKS :)
+        -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="//assets.locaweb.com.br/locastyle/2.0.6/javascripts/locastyle.js"></script>
+        <!--
+          JAVASCRIPT DO PRE-LOADER
+        -->
+        <script>
 
-	 <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-	 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-	</body>
+          //<![CDATA[
+          $(window).on('load', function () {
+            $('#preloader .inner').fadeOut();
+            $('#preloader').delay(350).fadeOut('slow'); 
+            $('body').delay(350).css({'overflow': 'visible'});
+          })
+          //]]>
+          
+          </script>
+      
+        
+      </footer> 	
+</body>
 
 	</html>
